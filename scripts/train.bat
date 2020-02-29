99f92b119ce7c79222a2d873c54f4e8a70317b56
SivilTaram <qianlxc@outlook.com>
2020-02-29 09:02:30 +0800

    Add training script on windows.

---
+++ scripts/train.bat
@@ -0,0 +1,18 @@
+cd ..
+
+set PARAM_SET=tiny
+set DATA_TRAIN=data/train.txt
+set DATA_DEV=data/dev.txt
+set MODEL_DIR=transformer/model_%$PARAM_SET%
+set VOCAB_FILE=resource/vocab.txt
+set BERT_CHECKPOINT=None
+
+set BLEU_SOURCE=%DATA_DEV%
+set BLEU_REF=data/BLEU_REF.txt
+
+set TRAIN_EPOCHS=50
+
+python transformer_main.py --data_train=%DATA_TRAIN% ^
+    --data_dev=%DATA_DEV% --model_dir=%MODEL_DIR% --vocab_file=%VOCAB_FILE% ^
+    --param_set=%PARAM_SET% --bleu_source=%BLEU_SOURCE% --bleu_ref=%BLEU_REF% ^
+    --bert_checkpoint=%BERT_CHECKPOINT% --train_epochs=%TRAIN_EPOCH%
